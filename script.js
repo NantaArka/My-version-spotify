a1036d43487babfc1eb5b8f385a0c68e8644fd27
NantaArka <athaarkananta1111@gmail.com>
2026-04-01 21:39:45 +0700

    Add import song button
    Add song display list element & import song button from explorer

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('log has called');
+
+// Test import song button and debug it
+const importSong = document.getElementById('fileInput');
+importSong.addEventListener("change", () => {
+    const file = importSong.files[0];
+    if (file) {
+        console.log("File dipilih:", file.name);
+        console.log("Tipe file:", file.type);
+        console.log("Ukuran file:", file.size, "bytes");
+    } else {
+        console.log("Tidak ada file yang dipilih");
+    }
+});
