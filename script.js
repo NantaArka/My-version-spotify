f1d6e6b248ea552b3f1223ecc0296372b6abb21d
NantaArka <athaarkananta1111@gmail.com>
2026-04-03 15:16:02 +0700

    Add song element into playlist display
    Add song element into playlist display

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const fileInput = document.getElementById("fileInput");
 
 ["dragenter", "dragover"].forEach(eventName => {
     dropArea.addEventListener(eventName, () => {
-        dropArea.classList.add("highlight");
+      dropArea.classList.add("highlight");
   });
 });
 
 ["dragleave", "drop"].forEach(eventName => {
     dropArea.addEventListener(eventName, () => {
-        dropArea.classList.remove("highlight");
+      dropArea.classList.remove("highlight");
   });
 });
 
@@ -22,26 +22,41 @@ dropArea.addEventListener("drop", (e) => {
   const files = e.dataTransfer.files;
 
   if (files.length > 0) {
-        fileInput.files = files;
-        handleFiles(files);
+    fileInput.files = files;
+    handleFiles(files);
   }
 });
 
 fileInput.addEventListener("change", () => {
-    handleFiles(fileInput.files);
+  handleFiles(fileInput.files);
 });
 
 function handleFiles(files) {
+  let hasInvalidFiles = false;
   [...files].forEach(file => {
     if (!file.type.startsWith("audio/")) {
-        alert("Must be audio file");
-        return;
+      hasInvalidFiles = true;      
+      return;
     }
+    const playlistParent = document.getElementById("playlist");
+    const div = document.createElement("div");
+    const image = document.createElement("img");
+    div.textContent = file.name;
+    div.classList.add("song-item");
+    image.src = "Image/Music_logo.png";
+    image.classList.add("song-image");
+    playlistParent.appendChild(div);
+    div.appendChild(image);
     console.log("File:", file.name);
+    console.log("Create song playlist elements");
   });
+  
+  if (hasInvalidFiles) {
+    alert("Must be audio file");
+  }
 }
 
 // music controller function
 function shuffleBtn() {
-    console.log('shuffle active');
+  console.log("shuffle active");
 }
